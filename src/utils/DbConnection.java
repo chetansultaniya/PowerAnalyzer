bbb3daf4fb03b1fa140cdf4a25e077aa2a91a3d5
prashantmaywad <prashantmaywad@gmail.com>
2018-03-16 11:07:47 +0530

    csv

--- src/utils/DbConnection.java
+++ src/utils/DbConnection.java
@@ -8,9 +8,9 @@ public class DbConnection {
 
 	public static Connection getDbConnection()
 	{
-		String dburl="jdbc:mysql://localhost:1010/poweranalyzer";
+		String dburl="jdbc:mysql://localhost:3306/poweranalyzer";
 		String  uname="root";
-		String  upass="root";
+		String  upass="7869";
 	        try
 	        {
 	            Class.forName("com.mysql.jdbc.Driver");
